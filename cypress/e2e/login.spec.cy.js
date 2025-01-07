36d2221e59a6aec94af5882bd7324de9f93068ea
Gabriel Galvão <gabriel.sog@gmail.com>
2025-01-07 14:47:15 -0300

    transactions test cases added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,5 @@
-describe('New user register sucess', () => {
-  it('Should register a new user with valid info', () => {
+describe('New User Registration Success', () => {
+  it('Should register a new user with valid information', () => {
     cy.visit('http://localhost:3000/')
     cy.get("[data-test='signup']").click()
     cy.get("#firstName").type('Alex')
@@ -9,25 +9,23 @@ describe('New user register sucess', () => {
     cy.get("#confirmPassword").type('test123')
     cy.get("[type='submit']").click()
     cy.get('.SignInForm-logo').should('be.visible')
-
   });
 });
 
-describe('Try to register a new user with incomplete informations', () => {
-  it('The button should not be available if all the fields are not filled correctly', () => {
+describe('Register with Incomplete Information', () => {
+  it('The submit button should be disabled if not all fields are filled correctly', () => {
     cy.visit('http://localhost:3000/')
     cy.get("[data-test='signup']").click()
     cy.get("#firstName").type('Axl')
     cy.get("#lastName").type('Rose')
     cy.get("#password").type('test123')
     cy.get("#confirmPassword").type('test123')
-    cy.get("[type='submit']").should('be.disabled');
+    cy.get("[type='submit']").should('be.disabled')
   });
 });
 
-describe('Login sucess', () => {
-  it('Should Login with valid user', () => {
-
+describe('Login Success', () => {
+  it('Should login with valid credentials', () => {
     cy.visit('http://localhost:3000/')
     cy.get('#username').type('Baba Yaga')
     cy.get('#password').type('test123')
@@ -36,8 +34,8 @@ describe('Login sucess', () => {
   });
 });
 
-describe('Login with invalid credentials', () => {
-  it('A error message should appears when try to login with a invalid user', () => {
+describe('Login with Invalid Credentials', () => {
+  it('An error message should appear when trying to login with invalid credentials', () => {
     cy.visit('http://localhost:3000/')
     cy.get('#username').type('Corey')
     cy.get('#password').type('Slipknot')
@@ -45,4 +43,3 @@ describe('Login with invalid credentials', () => {
     cy.get("[role='alert']").should('include.text', 'Username or password is invalid')
   });
 });
-
--- cypress/e2e/transactions.spec.cy.js
+++ cypress/e2e/transactions.spec.cy.js
@@ -1,18 +1,97 @@
 describe('Transfer money with sufficient balance', () => {
-    it('Should tranfer the money with success', () => {
+    it('Should transfer 50 and update the balance correctly', () => {
+
+        let balanceBefore;
+        let balanceAfter;
+
+        cy.visit('http://localhost:3000/')
+        cy.get('#username').type('Dina20')
+        cy.get('#password').type('s3cret')
+        cy.get("[type='submit']").click()
+
+        cy.get("[data-test='sidenav-user-balance']")
+            .invoke('text')
+            .then((capturedText) => {
+                const cleanedText = capturedText.replace(/[^\d.-]/g, '').trim();
+                balanceBefore = parseFloat(cleanedText);
+                cy.log(`Balance before transfer: ${balanceBefore}`);
+            });
+
+        cy.get("[data-test='nav-top-new-transaction']").click();
+        cy.get("[data-test='user-list-item-uBmeaz5pX']").click({ force: true });
+        cy.get("[name='amount']").type('50');
+        cy.get("[placeholder='Add a note']").type('Test Transfer');
+        cy.get("[data-test='transaction-create-submit-payment']").click();
+        cy.get('[data-test="alert-bar-success"]').should('be.visible').and('contain', 'Transaction Submitted!');
+
+        cy.wait(1000)
+
+        cy.get("[data-test='sidenav-user-balance']")
+            .invoke('text')
+            .then((capturedText) => {
+                const cleanedText = capturedText.replace(/[^\d.-]/g, '').trim();
+                balanceAfter = parseFloat(cleanedText);
+                cy.log(`Balance after transfer: ${balanceAfter}`);
+
+                const expectedBalance = balanceBefore - 50;
+                expect(balanceAfter).to.equal(expectedBalance);
+            });
+    });
+});
+
+describe('Send money with insufficient balance', () => {
+    it('Should display an error message when trying to send money with insufficient balance', () => {
+
+        let balanceBefore;
+
         cy.visit('http://localhost:3000/')
         cy.get('#username').type('Baba Yaga')
         cy.get('#password').type('test123')
         cy.get("[type='submit']").click()
-        cy.get('.NavBar-logo').should('be.visible')
+        cy.wait(1500)
 
-        // Passar pelo formulario de novos usuarios
-        // cy.get("[type='button']").eq(2).click()
-        // cy.get("[name='bankName']").type('Test Bank')
-        // cy.get("[name='routingNumber']").type('555666555')
-        // cy.get("[name='accountNumber']").type('198419841')
-        // cy.get("[type='submit']").click()
-        // cy.get("[type='button']").eq(2).click()
+        cy.get('body').then(($body) => {
+            if ($body.find('[data-test="user-onboarding-dialog-title"]').length > 0) {
+                cy.get("[type='button']").eq(2).click();
+                cy.get("[name='bankName']").type('Test Bank');
+                cy.get("[name='routingNumber']").type('555666555');
+                cy.get("[name='accountNumber']").type('198419841');
+                cy.get("[type='submit']").click();
+                cy.get("[type='button']").eq(2).click();
+                cy.get("[data-test='nav-top-new-transaction']").click();
+                cy.get("[data-test='user-list-item-uBmeaz5pX']").click({ force: true });
+                cy.get("[name='amount']").type('50');
+                cy.get("[placeholder='Add a note']").type('Test Transfer');
+                cy.get("[data-test='transaction-create-submit-payment']")
+                    .click()
+                    .get('[data-test="alert-bar-success"]')
+                    .wait(3000)
+                    .should('not.exist')
+            } else {
+                cy.log('Onboarding dialog was not found.');
+
+                cy.get("[data-test='sidenav-user-balance']")
+                    .invoke('text')
+                    .then((capturedText) => {
+                        const cleanedText = capturedText.replace(/[^\d.-]/g, '').trim();
+                        balanceBefore = parseFloat(cleanedText);
+                        cy.log(`Balance before attempted transfer: ${balanceBefore}`);
+
+                        if (balanceBefore === 0) {
+                            cy.log('Insufficient balance to complete the transfer.');
+                        }
+                    });
+
+                cy.get("[data-test='nav-top-new-transaction']").click();
+                cy.get("[data-test='user-list-item-uBmeaz5pX']").click({ force: true });
+                cy.get("[name='amount']").type('50');
+                cy.get("[placeholder='Add a note']").type('Test Transfer');
+                cy.get("[data-test='transaction-create-submit-payment']")
+                    .click()
+                    .get('[data-test="alert-bar-success"]')
+                    .wait(3000)
+                    .should('not.exist')
+            }
+        });
     });
 });
-
